65acde49a8ed9282548c878374413623abaabe3a
Minjeong Kang <alswjd4047@naver.com>
2019-10-11 20:15:42 +0900

    add uncommitted files...so sorry...
    Signed-off-by: Minjeong Kang <alswjd4047@naver.com>

---
+++ app/src/main/java/com/example/woo/myapplication/ui/activity/EmptyRoomActivity.java
@@ -0,0 +1,22 @@
+package com.example.woo.myapplication.ui.activity;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.woo.myapplication.R;
+
+public class EmptyRoomActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emptyroom);
+
+
+    }
+
+}
+
+
+
